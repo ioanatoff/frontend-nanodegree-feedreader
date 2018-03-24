fb51983c5b1304917db26205911c4f67bfbf335e
Anca Ioana Tofan <acnu04@gmail.com>
2018-03-24 13:42:21 +0200

    Added tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,15 +32,31 @@ $(function() {
          * and that the URL is not empty.
          */
 
+        it('have URLs', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('have names', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+        });
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -48,25 +64,82 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+        it('should be hidden by default', function(){
+            expect(document.querySelector('body').className).toBe('menu-hidden');
+        });
+
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+        it('should be show and hide on click', function(){
+            const menu = document.querySelector('.menu-icon-link');
+            var clickEvent = new MouseEvent('click', {
+                view: window,
+                bubbles: true,
+                cancelable: true
+            });
+            menu.dispatchEvent(clickEvent);
+            expect(document.querySelector('body').className).toBe('');
+            menu.dispatchEvent(clickEvent);
+            expect(document.querySelector('body').className).toBe('menu-hidden');
+        });
+
+    });
+
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+    describe('Initial Entries', function() {
+
+    /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('are loaded', function(done) {
+            const feed = document.querySelector('.feed');
+            expect(feed).toBeDefined();
+            expect(feed.children).toBeDefined();
+            expect(feed.children.length).toBeGreaterThan(0);
+            done();
+        });
+
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let content;
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                content = document.querySelector('.feed').textContent;
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+
+        it('changes content', function(done) {
+            const newContent = document.querySelector('.feed').textContent;
+            expect(content).toBeDefined();
+            expect(newContent).toBeDefined();
+            expect(content).not.toBe(newContent);
+            done();
+        });
+
+    });
 }());
